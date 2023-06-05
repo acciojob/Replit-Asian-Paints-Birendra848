0c899b544c30894a1751e46c1f60fe0bb932dad7
BIRENDRA KUMAR <75802334+Birendra848@users.noreply.github.com>
2023-06-05 12:32:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  var gridContainer = document.getElementById('grid-container');
+  var gridItems = document.querySelectorAll('.item');
+  var blockIdInput = document.getElementById('block_id');
+  var colorInput = document.getElementById('colour_id');
+  var changeButton = document.getElementById('change_button');
+  var resetButton = document.getElementById('reset_button');
+
+  function resetGrid() {
+    gridItems.forEach(function(item) {
+      item.style.backgroundColor = 'transparent';
+    });
+  }
+
+  function changeColor() {
+    var blockId = blockIdInput.value;
+    var color = colorInput.value;
+
+    var blockElement = document.getElementById(blockId);
+    if (blockElement) {
+      resetGrid();
+      blockElement.style.backgroundColor = color;
+    }
+  }
+
+  changeButton.addEventListener('click', changeColor);
+  resetButton.addEventListener('click', resetGrid);
+});
